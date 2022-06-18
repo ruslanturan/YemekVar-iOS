18ed793f750f48d8599f1fffc4cb8e15cffac6b3
ruslanturan <80384643+ruslanturan@users.noreply.github.com>
2022-06-18 17:25:38 +0400

    Delete Pods directory

--- Pods/Target Support Files/Pods-YemakVariOS-YemakVariOSUITests/Pods-YemakVariOS-YemakVariOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YemakVariOS_YemakVariOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_YemakVariOS_YemakVariOSUITests
-@end
--- Pods/Target Support Files/SIFloatingCollection/SIFloatingCollection-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SIFloatingCollection : NSObject
-@end
-@implementation PodsDummy_SIFloatingCollection
-@end
